1273717664e8c6f14353b446846992330d02b719
Veronica Grace Taleon <veronicagracetaleon@Veronicas-iMac.local>
2023-06-26 16:52:49 -0400

    IntelliJ project setup with DB connection

---
+++ server/annotations/src/main/java/org/example/models/Annotation.java
@@ -0,0 +1,24 @@
+package org.example.models;
+
+import java.time.LocalDate;
+
+public class Annotation {
+
+    private int annotationId;
+
+    private int appUserId;
+
+    private int bookId;
+
+    private LocalDate date;
+
+    private String quote;
+
+    private String annotation;
+
+    public Annotation() {
+
+    }
+
+
+}
